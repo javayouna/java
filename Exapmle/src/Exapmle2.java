99371861899ec733c6e14db4eab2c51bfb35775e
javayouna <106093094+javayouna@users.noreply.github.com>
2022-05-26 18:15:17 +0900

    Add files via upload

---
+++ Exapmle/src/Exapmle2.java
@@ -0,0 +1,39 @@
+
+public class Exapmle2 {
+
+	public static void main(String[] args) {
+		/*
+		 사용자가 입력한 숫자에 맞춰서 구구단이 작동되며,
+		 총합을 계산하여 짝수인지 홀수인지 출력하세요.
+		 "구구단 숫자를 입력하세요?"라고 메세지가 출력되면 
+		 해당 숫자에 맞춰서 모든 결과를 더하며,
+		 더한 값에 따라 짝수인지 홀수인지를 출력하시면 됩니다.
+		 단, 해당 반복문은 for~do~while문까지 한가지~~!
+		 */
+			
+		
+		 Scanner yy = new scanner(System.in);
+
+		 int a,aa;
+		 int total=0;
+		 int user;
+		 
+		  user=yy.nextint();
+          for(a=1;a<=9;a++);
+          for(us)
+          
+
+		 
+		 
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	}
+
+}

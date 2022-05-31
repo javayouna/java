da70905666f1c1f748a722da1cea1de9e324731d
javayouna <106093094+javayouna@users.noreply.github.com>
2022-05-31 09:40:26 +0900

    Add files via upload

---
+++ Array1/src/Array3.java
@@ -0,0 +1,59 @@
+import java.util.Scanner;
+
+public class Array3 {
+
+	public static void main(String[] args) {
+
+		/*
+		 * int num[] = {5,10,15,20,25,30,35}; /*for each문을 사용 할 때는 인덱스 번호 필요없이 사용 할 경우
+		 * -> 단순한 배열에 씀 for~do~while문 사용 할 때는 인덱스 번호 필요 할 떄 사용 -> 복잡한 배열에 씀 인덱스 번호:
+		 * [0][1][2]
+		 * 
+		 * for(int f:num) { //int f가 순차적으로 data배열값을 받아서 저장시킵니다. if(f%2==0)
+		 * System.out.println(f); }
+		 */
+
+	/*	String pay[] = { "무통장입금", "신용카드", "휴대폰", "상품권", "쿠폰" };
+		Scanner sc = new Scanner(System.in);
+	//	System.out.println("결제 형태를 선택해주세요");
+		String pm = sc.next();
+		for (String z : pay) { // ★배열 데이터가 문자이므로 받는 변수 형태 또한 문자형 사용해야 함
+			if (pm.equals(z)) {
+				if (z.equals("휴대폰")) {
+					//System.out.println("현재 시스템 점검으로 해당 결제는 사용 하실 수 없습니다.");
+
+				} else {
+					//System.out.println(pm + "로 결제 진행 됩니다.");
+				}
+
+				// System.out.println(pm+"로 결제 진행됩니다.");
+			}
+		}
+		sc.close();*/
+		
+/* 응용문제 
+ * {"햄버거", "피자", "치킨", "커피"}
+ * "주문하고자 하는 음식을 선택해주세요."
+ * 해당 질문은 총4번을 물어봄 
+ * 단, "주문종료"라고 사용자가 입력시 그 즉시 주문은 종료되며,
+ * 주문내역을 출력하시면 됩니다. 
+ */
+	 String food[]= {"햄버거", "피자", "치킨", "커피"};
+		
+		String msg = "주문하고자 하는 음식을 선택해주세요. 햄버거,피자,치킨,커피";
+		Scanner uu =new Scanner(System.in);
+		int sum=0;
+		for(int i=1;i<5;i++) {
+			System.out.println(msg);
+			String user=uu.next();
+			sum=user[i]+sum;
+	
+		}
+	
+		System.out.println("주문이 종료되었습니다. 주문내역은" + sum + "입니다");
+
+		uu.close();
+		
+	}
+
+}
--- Homework/src/Homework1.java
+++ Homework/src/Homework1.java
@@ -1,34 +1,34 @@
 import java.util.Scanner;
 
 public class Homework1 {
-	/*
-사용자가 입력한 숫자에 맞춰서 구구단이 작동되며,
-		 총합을 계산하여 짝수인지 홀수인지 출력하세요.
-		 "구구단 숫자를 입력하세요?"라고 메세지가 출력되면 
-		 해당 숫자에 맞춰서 모든 결과를 더하며,
-		 더한 값에 따라 짝수인지 홀수인지를 출력하시면 됩니다.
- */
+
 	public static void main(String[] args) {
-		Scanner gugu = new Scanner(System.in);
-		System.out.println("구구단 숫자를 입력하세요~");
-		int i = gugu.nextInt();
-		int e = 1;
-		int sum;
-		int total=0;
+		
+	/*	3개의 상품
+“바코드로 상품을 입력시켜주세요”
+금액을 입력 
+ “할인 %를 입력해 주세요.”라는 문구와 함께
+해당 할인 숫자를 입력하면
+ 최종금액이 변경 되어
+출력되도록 합니다.
+ */
 
-		for (e = 1; e < 9; e++) {
-			sum = e * i;
-			total = total + sum;
-			}
-
-			if (total % 2 == 0) {
-			System.out.println("짝수입니다.");
-			}
-
-			else if (total % 2 == 1) {
-			System.out.println("홀수입니다.");
-			}
-
+		   			
+		   	int f, ff;
+		   	int total;
+		   	for(f=1;f<=3;f++) {
+		   		//System.out.println(f);
+		   		for(ff=1;ff<=4;ff++) {
+		   	//	System.out.println(ff);
+		   		total=f+ff;
+		   		System.out.println(f+" + "+ff);
+		   		}
+		   	}
+		   			
+		   			
+		   			
+		   			
+		   			
 	}
 
 }
